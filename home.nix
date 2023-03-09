0df330290bdcc1917b25b97f7115e34b5cd3f42e
solidsnakedev <jona.ca.eng@gmail.com>
2023-03-09 12:14:46 -0500

    format config

--- home.nix
+++ home.nix
@@ -58,15 +58,15 @@
       {
         plugin = dracula;
         extraConfig = ''
-                          set -g @dracula-plugins "cpu-usage ram-usage network-bandwidth time weather"
-          				set -g @dracula-show-powerline true
-          			'';
+          set -g @dracula-plugins "cpu-usage ram-usage network-bandwidth time weather"
+          set -g @dracula-show-powerline true
+          '';
       }
     ];
     extraConfig = ''
       #Configure True Colors
       set -g default-terminal "screen-256color"
-      
+
       # Mouse works as expected
       set-option -g mouse on
 
@@ -79,7 +79,6 @@
       bind c new-window -c "#{pane_current_path}"
     '';
   };
-
 
   programs.neovim = {
     enable = true;
@@ -117,8 +116,10 @@
     ];
 
     plugins = with pkgs.vimPlugins; [
+
       # Basic settings
       sensible
+
       # Git support
       {
         plugin = lazygit-nvim;
@@ -127,6 +128,7 @@
           nmap("<leader>gg", ":LazyGit<cr>")
         '';
       }
+
       # Language support
       vim-nix
       {
@@ -140,10 +142,13 @@
           let g:haskell_enable_static_pointers = 1  " to enable highlighting of `static`
         '';
       }
-      # direnv
+
+      # direnv support
       direnv-vim
+
       # tmux integration
       vim-tmux-navigator
+
       # Comments
       {
         plugin = comment-nvim;
@@ -153,8 +158,9 @@
         '';
       }
 
-      # Syntax 
+      # Syntax Support
       (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars))
+
       # Buffer tabs
       {
         plugin = bufferline-nvim;
@@ -176,6 +182,7 @@
 
       # Icons Tree
       nvim-web-devicons
+
       # File Tree
       {
         plugin = nvim-tree-lua;
@@ -238,26 +245,86 @@
     extraConfig = ''
 
       let mapleader=" "
-      set nocompatible            " disable compatibility to old-time vi
-      set showmatch               " show matching 
-      set ignorecase              " case insensitive 
-      set mouse=v                 " middle-click paste with 
-      set hlsearch                " highlight search 
-      set incsearch               " incremental search
-      set tabstop=4               " number of columns occupied by a tab 
-      set softtabstop=4           " see multiple spaces as tabstops so <BS> does the right thing
-      set expandtab               " converts tabs to white space
-      set shiftwidth=4            " width for autoindents
+
+      " Disable compatibility with vi which can cause unexpected issues.
+      set nocompatible
+
+      " Enable type file detection. Vim will be able to try to detect the type of file in use.
+      filetype on
+
+      " Enable plugins and load plugin for the detected file type.
+      filetype plugin on
+
+      " Load an indent file for the detected file type.
+      filetype indent on
+
+      " Turn syntax highlighting on.
+      syntax on
+
+      " Add numbers to each line on the left-hand side.
+      set number
+
+      " Highlight cursor line underneath the cursor horizontally.
+      set cursorline
+
+      " Highlight cursor line underneath the cursor vertically.
+      set cursorcolumn
+
+      " Set shift width to 2 spaces.
+      set shiftwidth=2
+
+      " Set tab width to 2 columns.
+      set tabstop=2
+
+      " Use space characters instead of tabs.
+      set expandtab
+
+      " Do not save backup files.
+      set nobackup
+
+      " Do not let cursor scroll below or above N number of lines when scrolling.
+      set scrolloff=10
+
+      " Do not wrap lines. Allow long lines to extend as far as the line goes.
+      set nowrap
+
+      " While searching though a file incrementally highlight matching characters as you type.
+      set incsearch
+
+      " Ignore capital letters during search.
+      set ignorecase
+
+      " Override the ignorecase option if searching for capital letters.
+      " This will allow you to search specifically for capital letters.
+      set smartcase
+
+      " Show partial command you type in the last line of the screen.
+      set showcmd
+
+      " Show the mode you are on the last line.
+      set showmode
+
+      " Show matching words during a search.
+      set showmatch
+
+      " Use highlighting when doing a search.
+      set hlsearch
+
+      " Set the commands to save in history default number is 20.
+      set history=1000
+
+      " Enable auto completion menu after pressing TAB.
+      set wildmenu
+
+      " Make wildmenu behave like similar to Bash completion.
+      set wildmode=list:longest
+
+      set softtabstop=2           " see multiple spaces as tabstops so <BS> does the right thing
       set autoindent              " indent a new line the same amount as the line just typed
-      set number                  " add line numbers
-      set wildmode=longest,list   " get bash-like tab completions
-      filetype plugin indent on   "allow auto-indenting depending on file type
-      syntax on                   " syntax highlighting
       set mouse=a                 " enable mouse click
       set clipboard=unnamedplus   " using system clipboard
-      filetype plugin on
-      set cursorline              " highlight current cursorline
       set ttyfast                 " Speed up scrolling in Vim
+      set list listchars=tab:▸▸,trail:·
 
       " --------------------------------------------------------
       " COC-VIM TAB SETTINGS START
@@ -265,15 +332,15 @@
       " Some servers have issues with backup files, see #649
       set nobackup
       set nowritebackup
-      
+
       " Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
       " delays and poor user experience
       set updatetime=300
-      
+
       " Always show the signcolumn, otherwise it would shift the text each time
       " diagnostics appear/become resolved
       set signcolumn=yes
-      
+
       " Use tab for trigger completion with characters ahead and navigate
       " NOTE: There's always complete item selected by default, you may want to enable
       " no select by `"suggest.noselect": true` in your configuration file
@@ -284,38 +351,38 @@
             \ CheckBackspace() ? "\<Tab>" :
             \ coc#refresh()
       inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
-      
+
       " Make <CR> to accept selected completion item or notify coc.nvim to format
       " <C-g>u breaks current undo, please make your own choice
       inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
                                     \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
-      
+
       function! CheckBackspace() abort
         let col = col('.') - 1
         return !col || getline('.')[col - 1]  =~# '\s'
       endfunction
-      
+
       " Use <c-space> to trigger completion
       if has('nvim')
         inoremap <silent><expr> <c-space> coc#refresh()
       else
         inoremap <silent><expr> <c-@> coc#refresh()
       endif
-      
+
       " Use `[g` and `]g` to navigate diagnostics
       " Use `:CocDiagnostics` to get all diagnostics of current buffer in location list
       nmap <silent> [g <Plug>(coc-diagnostic-prev)
       nmap <silent> ]g <Plug>(coc-diagnostic-next)
-      
+
       " GoTo code navigation
       nmap <silent> gd <Plug>(coc-definition)
       nmap <silent> gy <Plug>(coc-type-definition)
       nmap <silent> gi <Plug>(coc-implementation)
       nmap <silent> gr <Plug>(coc-references)
-      
+
       " Use K to show documentation in preview window
       nnoremap <silent> K :call ShowDocumentation()<CR>
-      
+
       function! ShowDocumentation()
         if CocAction('hasProvider', 'hover')
           call CocActionAsync('doHover')
@@ -323,17 +390,17 @@
           call feedkeys('K', 'in')
         endif
       endfunction
-      
+
       " Highlight the symbol and its references when holding the cursor
       autocmd CursorHold * silent call CocActionAsync('highlight')
-      
+
       " Symbol renaming
       nmap <leader>rn <Plug>(coc-rename)
-      
+
       " Formatting selected code
       xmap <leader>f  <Plug>(coc-format-selected)
       nmap <leader>f  <Plug>(coc-format-selected)
-      
+
       augroup mygroup
         autocmd!
         " Setup formatexpr specified filetype(s)
@@ -341,27 +408,27 @@
         " Update signature help on jump placeholder
         autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
       augroup end
-      
+
       " Applying code actions to the selected code block
       " Example: `<leader>aap` for current paragraph
       xmap <leader>a  <Plug>(coc-codeaction-selected)
       nmap <leader>a  <Plug>(coc-codeaction-selected)
-      
+
       " Remap keys for applying code actions at the cursor position
       nmap <leader>ac  <Plug>(coc-codeaction-cursor)
       " Remap keys for apply code actions affect whole buffer
       nmap <leader>as  <Plug>(coc-codeaction-source)
       " Apply the most preferred quickfix action to fix diagnostic on the current line
       nmap <leader>qf  <Plug>(coc-fix-current)
-      
+
       " Remap keys for applying refactor code actions
       nmap <silent> <leader>re <Plug>(coc-codeaction-refactor)
       xmap <silent> <leader>r  <Plug>(coc-codeaction-refactor-selected)
       nmap <silent> <leader>r  <Plug>(coc-codeaction-refactor-selected)
-      
+
       " Run the Code Lens action on the current line
       nmap <leader>cl  <Plug>(coc-codelens-action)
-      
+
       " Map function and class text objects
       " NOTE: Requires 'textDocument.documentSymbol' support from the language server
       xmap if <Plug>(coc-funcobj-i)
@@ -372,7 +439,7 @@
       omap ic <Plug>(coc-classobj-i)
       xmap ac <Plug>(coc-classobj-a)
       omap ac <Plug>(coc-classobj-a)
-      
+
       " Remap <C-f> and <C-b> to scroll float windows/popups
       if has('nvim-0.4.0') || has('patch-8.2.0750')
         nnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
@@ -382,21 +449,21 @@
         vnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
         vnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
       endif
-      
+
       " Use CTRL-S for selections ranges
       " Requires 'textDocument/selectionRange' support of language server
       nmap <silent> <C-s> <Plug>(coc-range-select)
       xmap <silent> <C-s> <Plug>(coc-range-select)
-      
+
       " Add `:Format` command to format current buffer
       command! -nargs=0 Format :call CocActionAsync('format')
-      
+
       " Add `:Fold` command to fold current buffer
       command! -nargs=? Fold :call     CocAction('fold', <f-args>)
-      
+
       " Add `:OR` command for organize imports of the current buffer
       command! -nargs=0 OR   :call     CocActionAsync('runCommand', 'editor.action.organizeImport')
-      
+
       " Mappings for CoCList
       " Show all diagnostics
       nnoremap <silent><nowait> <space>a  :<C-u>CocList diagnostics<cr>
