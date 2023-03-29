966e681a70ce1ba94664123cc17c5e206fd812f9
solidsnakedev <jona.ca.eng@gmail.com>
2023-03-29 09:06:49 -0400

    improve wilder menu

--- home.nix
+++ home.nix
@@ -263,19 +263,18 @@
 
           wilder.set_option('renderer', wilder.popupmenu_renderer(
             wilder.popupmenu_border_theme({
+              highlighter = wilder.basic_highlighter(),
               highlights = {
                 border = 'Normal', -- highlight to use for the border
+                accent = wilder.make_hl('WilderAccent', 'Pmenu', {{a = 1}, {a = 1}, {foreground = '#f4468f'}}),
               },
               -- 'single', 'double', 'rounded' or 'solid'
               -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
               border = 'rounded',
+              left = {' ', wilder.popupmenu_devicons()},
+              right = {' ', wilder.popupmenu_scrollbar()},
             })
-          )) 
-          wilder.set_option('renderer', wilder.popupmenu_renderer({
-            highlighter = wilder.basic_highlighter(),
-            left = {' ', wilder.popupmenu_devicons()},
-            right = {' ', wilder.popupmenu_scrollbar()},
-          }))
+            ))
         '';
       }
 
