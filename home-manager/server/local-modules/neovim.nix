77fcb74c39bef31a2e9b4bf5174740430f8bd12a
solidsnakedev <jona.ca.eng@gmail.com>
2023-05-19 09:10:34 -0400

    new flake.lock , enable classic haskell highlight

--- home-manager/server/local-modules/neovim.nix
+++ home-manager/server/local-modules/neovim.nix
@@ -101,12 +101,7 @@ in
       {
         plugin = haskell-vim;
         config = ''
-          let g:haskell_enable_quantification = 1   " to enable highlighting of `forall`
-          let g:haskell_enable_recursivedo = 1      " to enable highlighting of `mdo` and `rec`
-          let g:haskell_enable_arrowsyntax = 1      " to enable highlighting of `proc`
-          let g:haskell_enable_pattern_synonyms = 1 " to enable highlighting of `pattern`
-          let g:haskell_enable_typeroles = 1        " to enable highlighting of type roles
-          let g:haskell_enable_static_pointers = 1  " to enable highlighting of `static`
+          let g:haskell_classic_highlighting = 1
         '';
       }
 
@@ -153,6 +148,8 @@ in
           require("nvim-treesitter.configs").setup({
             highlight = {
               enable = true,
+              disable = {"haskell"},
+              additional_vim_regex_highlighting = false,
             },
           })
         '';
