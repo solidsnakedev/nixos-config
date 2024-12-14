db652ab3080e38194784a397f8891af45a00b244
solidsnakedev <jona.ca.eng@gmail.com>
2024-12-14 09:11:27 -0700

    refactor: clean

--- home-manager/mac/home.nix
+++ home-manager/mac/home.nix
@@ -98,26 +98,6 @@
 
   };
 
-  # programs.zsh = {
-  #   enable = true;
-  #   shellAliases = {
-  #     ll = "ls -l";
-  #     # update = "sudo nixos-rebuild switch";
-  #   };
-  #   history = {
-  #     size = 10000;
-  #     path = "${config.xdg.dataHome}/zsh/history";
-  #   };
-  #   enableVteIntegration = true;
-  #   # zplug = {
-  #   #   enable = true;
-  #   #   plugins = [
-  #   #     { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
-  #   #     { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
-  #   #   ];
-  #   # };
-  # };
-
   xdg.configFile.yabai = {
     source = ../../config/yabai;
     recursive = true;
