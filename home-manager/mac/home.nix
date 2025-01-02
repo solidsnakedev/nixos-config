c04ab68e8f6d033f5ee4b3bfd18aff9faf51ac3f
solidsnakedev <jona.ca.eng@gmail.com>
2025-01-02 11:12:23 -0700

    refactor: remove kitty and wezterm

--- home-manager/mac/home.nix
+++ home-manager/mac/home.nix
@@ -42,39 +42,6 @@
       gh
       eza
     ];
-  programs.wezterm = {
-    enable = true;
-  };
-
-  programs.kitty = {
-    enable = false;
-    shellIntegration.enableFishIntegration = true;
-    darwinLaunchOptions = [
-      "--single-instance"
-    ];
-    font.name = "JetBrainsMono Nerd Font";
-    font.size = 14;
-    themeFile = "Catppuccin-Mocha";
-    settings = {
-      hide_window_decorations = "yes";
-      shell = "${pkgs.fish}/bin/fish";
-      window_padding_width = 5;
-      background_opacity = 0.95;
-      enable_audio_bell = false;
-    };
-    keybindings = {
-      "cmd+backspace" = "send_key ctrl+u"; # ⌘⌫
-      # Move to end of line
-      "cmd+right" = "send_key ctrl+e";
-      # Move to start of line
-      "cmd+left" = "send_key ctrl+a";
-    };
-    extraConfig = ''
-      # Nerd Fonts v3.2.0
-      symbol_map U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d7,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b1,U+e700-U+e7c5,U+ed00-U+efc1,U+f000-U+f2ff,U+f000-U+f2e0,U+f300-U+f372,U+f400-U+f533,U+f0001-U+f1af0 Symbols Nerd Font Mono
-
-    '';
-  };
 
   programs.alacritty = {
     enable = true;
